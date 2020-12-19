6695a424339d808da37dda2ba50fb0a11c701a0f
crane0221 <75875765+crane0221@users.noreply.github.com>
2020-12-19 16:22:20 +0800

    Add files via upload

---
+++ sqlmy/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using(SqlConnection cn = cn1.cnn)
+            {
+                cn.Open();
+                SqlCommand con = new SqlCommand();
+                con.Connection = cn;
+                con.CommandText =                    
+                    $"Select Birthday From Account Where AccountID = '0000000005'";
+                MessageBox.Show(con.ExecuteScalar().ToString());
+                //SqlDataReader dr = con.ExecuteReader();
+                //while (dr.Read())
+                //{
+                //    if (! dr.Equals(DBNull.Value))
+                //    {
+                //        comboBox1.Items.Add(dr[0].ToString());
+                //    }
+                //}
+            }
+        }
+    }
+}
---
+++ sqlmy/WindowsFormsApp1/cn1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public static class cn1
+    {
+        public static SqlConnection cnn
+        {
+            get
+            {
+                return new SqlConnection("Data Source=.;Initial Catalog=Disneyland_Shanghai_ANO;Integrated Security=True");
+            }
+        }
+    }
+}
